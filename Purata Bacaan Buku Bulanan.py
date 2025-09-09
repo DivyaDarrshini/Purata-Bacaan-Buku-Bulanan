c69c3e0d4f91bdad3ca767b836432ad45a322924
DivyaDarrshini <darrshinidivya@yahoo.com>
2025-09-09 12:55:01 +0800

    Update Purata Bacaan Buku Bulanan.py

--- Purata Bacaan Buku Bulanan.py
+++ Purata Bacaan Buku Bulanan.py
@@ -1,24 +1,62 @@
-import chess
-board = chess.Board()
-board.legal_moves
-LegalMoveGenerator at ... (Nh3, Nf3, Nc3, Na3, h3, g3, f3, e3, d3, c3, ...)
-chess.Move.from_uci("a8a1") in board.legal_moves
-False
-board.push_san("e4")
-Move.from_uci('e2e4')
-board.push_san("e5")
-Move.from_uci('e7e5')
-board.push_san("Qh5")
-Move.from_uci('d1h5')
-board.push_san("Nc6")
-Move.from_uci('b8c6')
-board.push_san("Bc4")
-Move.from_uci('f1c4')
-board.push_san("Nf6")
-Move.from_uci('g8f6')
-board.push_san("Qxf7")
-Move.from_uci('h5f7')
- board.is_checkmate()
-True
-board
-Board('r1bqkb1r/pppp1Qpp/2n2n2/4p3/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 4')
+from tkinter import *
+
+fields = ('Bilangan Buku', 'Bilangan Hari')
+def purata (entries):
+ x = int (entries ['Bilangan Buku').get())
+ y = int (entries ['Bilangan Hari'].get())
+
+if x < 1:
+ print("Masukkan bilangan buku melebihi 1")
+if 29 > y or 31 < y:
+ print("Masukkan bilangan hari antara 29 hingga 31")
+elif y == 31 or 30 or 29:
+ z = x/y 
+ a = round (z,2)
+ purata = print("Purata Bacaan Buku pada Setiap Bulan:", a)
+ 
+ if a > 5 :
+  print("Tahap IQ: Lebih daripada 140")
+  print("Kelasifikasi: Pintar")
+  
+elif 4 <= a <= 5 :
+ print ("Tahap IQ: 120 hingga 140") 
+ print("Kelasifikasi: Baik")
+
+elif 3 <= a <= 4 :
+ print("Tahap IQ: 110 hingga 120") 
+ print ("Kelasifikasi: Sederhana")
+ 
+elif 2 <= a <= 3 :
+ print("Tahap IQ: 90 hingga 110") 
+ print("Kelasifikasi: Memuaskan")
+ 
+elif 1 <= a <= 2 :
+ print("Tahap IQ: 80 hingga 90") 
+ print("Kelasifikasi: Kurang memuaskan")
+ 
+else:
+ print("Tahap IQ: Kurang daripada 80") 
+ print("Kelasifikasi: Tidak memuaskan")
+
+def makeform(root, fields):
+ entries = {}
+ for field in fields:
+  row = Frame(root)
+  lab = Label(row, width=22, text=field+": ", anchor='w')
+  ent = Entry(row)
+  ent.insert(0,"0")
+  row.pack(side = TOP, fill = X, padx = 5, packy = 5)
+  lab.pack(side = LEFT)
+  ent.pack(side = RIGHT, expand = YES, fill = X)
+  entries[field] = ent
+return entries
+
+if __name__ == '__main__':
+ root = Tk()
+ root.title("Pengiraan Purata Bacaan Buku pada Setiap Bulan")
+ ents = makeform(root, fields)
+ root.bind('<Return>', (lambda event, e ents: fetch(e)))
+ bl =  Button(root, text = 'Kira', command=(lambda e = ents: purata(e)))
+ bl.pack(side = LEFT, padx = 5, pady = 5)
+ root.mainloop()
+
